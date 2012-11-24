90215d93cb3c5f32434651718e4e673d506cbe9a
Elliot Hughes <elliot.hughes@gmail.com>
2012-11-24 23:11:59 +0000

    Fixed bug patrick#1

--- data/membersToSql.py
+++ data/membersToSql.py
@@ -11,12 +11,16 @@ with sqlite3.connect('data.db') as conn:
     c = conn.cursor()
     c.execute('''drop table if exists mps''')
     c.execute('''create table mps (title text, firstname text, lastname text, constituency text)''')
+    constituencies_seen = []
     for member in parliament:
-        c.execute('''insert into mps values (?, ?, ?, ?)''', 
-                  (member.attrib['constituency'],
-                   member.attrib['title'],
-                   member.attrib['firstname'],
-                   member.attrib['lastname']))
+        if member.attrib['constituency'] not in constituencies_seen:
+            c.execute('''replace into mps values (?, ?, ?, ?)''', 
+                      (
+                    member.attrib['title'],
+                    member.attrib['firstname'],
+                    member.attrib['lastname'],
+                    member.attrib['constituency']))
+            constituencies_seen.append(member.attrib['constituency'])
     conn.commit()
 
         
