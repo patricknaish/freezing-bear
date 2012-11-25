d13f76fa13626e36f265bfeec6e2491acb9cd63a
Patrick Naish <patrick@pmnaish.co.uk>
2012-11-25 05:33:05 +0000

    Created ranking by expenses claimed (desc)

--- server/src/TemplateParser.java
+++ server/src/TemplateParser.java
@@ -21,42 +21,66 @@ public class TemplateParser {
 	public Object[] parse() {
 		ArrayList<String> output = new ArrayList<String>();
 		String input;
-		String match;
+		String match, path, method;
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			while ((input = reader.readLine()) != null) {
 				if (input.contains("<%") && input.contains("%>")) {
 					match = input.split("<%")[1];
 					match = match.split("%>")[0];
-					DatabaseConnector dbc = new DatabaseConnector(match);
-					ResultSet results = dbc.query("SELECT * FROM constituencies, mps "+
-												  "WHERE constituencies.name = mps.constituency "+
-												  "ORDER BY constituencies.name ASC");
-					output.add("[");
-					while (results.next()) {
-						output.add("{name:\""+results.getString("name")+"\", " +
-							       "lat:\""+results.getString("lat")+"\", " +
-							       "lon:\""+results.getString("lon")+"\", " +
-								   "population:\""+results.getString("population")+"\", " +
-								   "outOfWorkLevel:\""+results.getString("outofwork_level")+"\", " +
-								   "outOfWorkRate:\""+results.getString("outofwork_rate")+"\", " +
-								   "incapacityLevel:\""+results.getString("incap_level")+"\", " +
-								   "incapacityRate:\""+results.getString("incap_rate")+"\", " +
-								   "medianWage:\""+results.getString("medianwage")+"\", " +
-								   "mpName:\""+results.getString("title")+" "+results.getString("firstname")+" "+results.getString("lastname")+"\", " +
-								   "mpExpenses:\""+results.getString("expense")+"\", " +
-								   "mpAttendance:\""+results.getString("attendance")+"\", " +
-								   "mpRebellion:\""+results.getString("rebellion")+"\", " +
-								   "mpImg:\"/img/"+results.getString("firstname")+"_"+results.getString("lastname")+".jpg\"},");
+					path = match.split(":")[0];
+					method = match.split(":")[1];
+					
+					if (method.equals("listAll")) {
+						DatabaseConnector dbc = new DatabaseConnector(path);
+						ResultSet results = dbc.query("SELECT * FROM constituencies, mps "+
+													  "WHERE constituencies.name = mps.constituency "+
+													  "ORDER BY constituencies.name ASC");
+						output.add("[");
+						while (results.next()) {
+							output.add("{name:\""+results.getString("name")+"\", " +
+								       "lat:\""+results.getString("lat")+"\", " +
+								       "lon:\""+results.getString("lon")+"\", " +
+									   "population:\""+results.getString("population")+"\", " +
+									   "outOfWorkLevel:\""+results.getString("outofwork_level")+"\", " +
+									   "outOfWorkRate:\""+results.getString("outofwork_rate")+"\", " +
+									   "incapacityLevel:\""+results.getString("incap_level")+"\", " +
+									   "incapacityRate:\""+results.getString("incap_rate")+"\", " +
+									   "medianWage:\""+results.getString("medianwage")+"\", " +
+									   "mpName:\""+results.getString("title")+" "+results.getString("firstname")+" "+results.getString("lastname")+"\", " +
+									   "mpExpenses:\""+results.getString("expense")+"\", " +
+									   "mpAttendance:\""+results.getString("attendance")+"\", " +
+									   "mpRebellion:\""+results.getString("rebellion")+"\", " +
+									   "mpImg:\"/img/"+results.getString("firstname")+"_"+results.getString("lastname")+".jpg\"},");
+						}
+						String last = output.get(output.size()-1);
+						last = last.substring(0,last.length()-1);
+						output.set(output.size() - 1, last); 
+						output.add("]");
 					}
-					String last = output.get(output.size()-1);
-					last = last.substring(0,last.length()-1);
-					output.set(output.size() - 1, last); 
-					output.add("]");
+					else if (method.equals("rankByExpenses")) {
+						DatabaseConnector dbc = new DatabaseConnector(path);
+						ResultSet results = dbc.query("SELECT * FROM constituencies, mps " +
+								  					  "WHERE constituencies.name = mps.constituency "+
+													  "ORDER BY expense DESC");
+						output.add("[");
+						while (results.next()) {
+							output.add("{mpName:\""+results.getString("title")+" "+results.getString("firstname")+" "+results.getString("lastname")+"\", " +
+									   "mpConstituency:\""+results.getString("name")+"\", " +
+									   "mpExpenses:\""+results.getString("expense")+"\", " +
+									   "mpAttendance:\""+results.getString("attendance")+"\", " +
+									   "mpRebellion:\""+results.getString("rebellion")+"\", " +
+									   "mpImg:\"/img/"+results.getString("firstname")+"_"+results.getString("lastname")+".jpg\"},");
+						}
+						String last = output.get(output.size()-1);
+						last = last.substring(0,last.length()-1);
+						output.set(output.size() - 1, last); 
+						output.add("]");
+					}
 					
 				}
 				else {
-					//output.add(input);
+					output.add(input);
 				}
 			}
 		} catch (FileNotFoundException e) {
